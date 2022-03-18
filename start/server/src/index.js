e6b22016d72bd63ffcf601ec0b25fdc20e17b9ca
Zachary Morel <zacharyjmorel@gmail.com>
2022-03-18 13:01:19 -0400

    Added LaunchAPI, adminding server start functions

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,8 +1,22 @@
+// Dependencies
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
-const typeDefs = require("./schema");
 
-const server = new ApolloServer({ typeDefs });
+// Relative imports
+const typeDefs = require("./schema");
+const { createStore } = require("./utils");
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
 
 server.listen().then(() => {
   console.log(`
