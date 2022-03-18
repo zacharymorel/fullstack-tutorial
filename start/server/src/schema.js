8922646b2d0e0909b3fcbfa7fd35da53a015c20f
Zachary Morel <zacharyjmorel@gmail.com>
2022-03-17 20:29:47 -0400

    Standing up Apollo server and schema

---
+++ start/server/src/schema.js
@@ -0,0 +1,43 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  # Your schema will go here
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+    token: String
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+`;
+
+module.exports = typeDefs;
