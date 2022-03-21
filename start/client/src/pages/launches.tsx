214d7dc1245baffe7328326c256a72683d21a455
Zachary Morel <zacharyjmorel@gmail.com>
2022-03-21 13:14:59 -0400

    Added Merging Cahcing, flushing out UI and xhr functionality

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,6 +1,11 @@
+// Dependencies
 import React, { Fragment, useState } from "react";
 import { RouteComponentProps } from "@reach/router";
-import { gql } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
+
+// Relative imports
+import { LaunchTile, Header, Button, Loading } from "../components";
+import * as GetLaunchListTypes from "./__generated__/GetLaunchList";
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -34,7 +39,46 @@ export const GET_LAUNCHES = gql`
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
+  const { data, error, fetchMore, loading } = useQuery<
+    GetLaunchListTypes.GetLaunchList,
+    GetLaunchListTypes.GetLaunchListVariables
+  >(GET_LAUNCHES);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))}
+
+      {data.launches &&
+        data.launches.hasMore &&
+        (isLoadingMore ? (
+          <Loading />
+        ) : (
+          <Button
+            onClick={async () => {
+              setIsLoadingMore(true);
+              await fetchMore({
+                variables: {
+                  after: data.launches.cursor,
+                },
+              });
+              setIsLoadingMore(false);
+            }}
+          >
+            Load More
+          </Button>
+        ))}
+    </Fragment>
+  );
 };
 
 export default Launches;
