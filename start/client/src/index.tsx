d54eff162db77f639e135210c53885457e27e104
Zachary Morel <zacharyjmorel@gmail.com>
2022-03-21 21:05:58 -0400

    Ending application, added cache mutiations to flush out functimonality

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,18 +1,46 @@
+// Dependencies
 import {
   ApolloClient,
-  NormalizedCacheObject,
   ApolloProvider,
+  gql,
+  NormalizedCacheObject,
+  useQuery,
 } from "@apollo/client";
-import { cache } from "./cache";
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Relatives
+import { cache } from "./cache";
 import Pages from "./pages";
 import injectStyles from "./styles";
+import Login from "./pages/login";
+
+export const typeDefs = gql`
+  extend type Query {
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+  }
+`;
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 // Initialize ApolloClient
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   uri: "http://localhost:4000/graphql",
+  headers: {
+    authorization: localStorage.getItem("token") || "",
+  },
+  typeDefs,
 });
 
 injectStyles();
@@ -20,7 +48,7 @@ injectStyles();
 // Pass the ApolloClient instance to the ApolloProvider component
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
